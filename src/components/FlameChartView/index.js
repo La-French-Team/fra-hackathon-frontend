6b23e5832a07a5c10c05e393a830fde123cb69cf
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2023-06-24 17:06:23 -0400

    update flamechart loading

--- src/components/FlameChartView/index.js
+++ src/components/FlameChartView/index.js
@@ -3,59 +3,64 @@ import { useSession } from "next-auth/react";
 import { FlameGraph } from "react-flame-graph";
 import useResizeObserver from "use-resize-observer";
 
-const data = {
-  name: "root",
-  value: 5,
-  children: [
-    {
-      name: "custom tooltip",
-      value: 1,
-
-      // Each node can specify a "tooltip" to be shown on hover.
-      // By default, the node's "name" will be used for this.
-      tooltip: "Custom tooltip shown on hover",
-    },
-    {
-      name: "custom colors",
-
-      // Each node can also provide a custom "backgroundColor" or text "color".
-      backgroundColor: "#35f",
-      color: "#fff",
-
-      value: 3,
-      children: [
-        {
-          name: "leaf",
-          value: 2,
-        },
-      ],
-    },
-  ],
+const loadingFlameGraph = {
+  name: "loading",
+  value: 1,
 };
 
+// 1. Service Request - ExecutionPlan
+// 2. Service - ExecutionPlan
+// 3. Activity - 1 Execution Plan - Sequence Number
+// 4. Loop
 function transform(results, session) {
   if (results == null) {
-    return {
-      name: "loading",
-      value: 1,
-    };
+    return loadingFlameGraph;
   }
-
-  // 1. Service Request - ExecutionPlan
-  // 2. Service - ExecutionPlan
-  // 3. Activity - 1 Execution Plan - Sequence Number
-  // 4. Loop
 
   /**
    * @type Array
    */
   const requests = results[0].requests;
-  const serviceRequest = requests.find((req) => req.type === "ServiceRequest");
-  const service = requests.find((req) => req.type === "Service");
-  const activities = requests
-    .filter((req) => req.type === "Activity")
-    .sort((req) => req.body.sequenceNumber);
 
+  // 1. Find the first ServiceRequest issued by the customer (seller)
+  const serviceRequest = requests.find(
+    (req) =>
+      req.type === "ServiceRequest" &&
+      req.body.parentActivities[0].includes("seller")
+  );
+  if (!serviceRequest) {
+    console.warn(
+      "Could not find ServiceRequest with parent activity that starts with seller"
+    );
+    return;
+  }
+
+  // 2. Find the service that related to the service request
+  const serviceId = stringAfter(serviceRequest.body.service, "/");
+  const service = findElementById("Service", serviceId, requests);
+  if (!service) {
+    console.warn("Could not find Service with id", serviceId);
+    return;
+  }
+
+  console.log("The service", service);
+  // 3. Find the linked activities
+  const activities = service.body.activities
+    .map((activityId) =>
+      findElementById("Activity", stringAfter(activityId, "/"), requests)
+    )
+    .sort((req) => req.body.sequenceNumber);
+  if (activities.length === 0) {
+    console.log(
+      "Found 0 activities related to service",
+      serviceId,
+      service.body.activities
+    );
+  }
+
+  console.log("the activities", activities);
+
+  // 4. Loop on the Service Requests from the activities
   const flamegraph = {
     name: formatName(serviceRequest, session),
     value: activities.length,
@@ -64,17 +69,109 @@ function transform(results, session) {
         name: formatName(service, session),
         value: activities.length,
         children: activities.map((activity) => {
-          return { name: formatName(activity, session), value: 1 };
+          return {
+            name: formatName(activity, session),
+            value: 1,
+            children: [
+              transformInner(
+                findElementById(
+                  "ServiceRequest",
+                  stringAfter(activity.params.id, "/"),
+                  requests
+                ),
+                requests,
+                session
+              ),
+            ],
+          };
         }),
       },
     ],
   };
 
+  console.log(flamegraph);
+
   return flamegraph;
 }
 
+function transformInner(serviceRequest, requests, session) {
+  if (!serviceRequest) {
+    return {
+      name: "empty",
+      value: 1,
+    };
+  }
+  // 1. Find the service that related to the service request
+  const serviceId = stringAfter(serviceRequest.body.service, "/");
+  const service = findElementById("Service", serviceId, requests);
+  if (!service) {
+    console.warn("Could not find Service with id", serviceId);
+    return {
+      name: formatName(serviceRequest, session),
+      value: 1,
+    };
+  }
+  console.log("Found service for", serviceId, service);
+
+  // 2. Find associated activities
+  const activities = service.body.activities
+    .map((activityId) =>
+      findElementById("Activity", stringAfter(activityId, "/"), requests)
+    )
+    .sort((req) => req.body.sequenceNumber);
+  if (activities.length === 0) {
+    console.log(
+      "Found 0 activities related to service",
+      serviceId,
+      service.body.activities
+    );
+  }
+
+  const flamegraph = {
+    name: formatName(serviceRequest, session),
+    value: 1,
+    children: service
+      ? [
+          {
+            name: formatName(service, session),
+            value: 1,
+            children: activities.map((activity) => {
+              transformInner(
+                findElementById(
+                  "ServiceRequest",
+                  stringAfter(activity.params.id, "/"),
+                  requests
+                ),
+                requests,
+                session
+              );
+            }),
+          },
+        ]
+      : [],
+  };
+
+  return flamegraph;
+}
+
+function findElementById(type, id, list) {
+  const found = list.find((req) => req.type === type && req.params?.id === id);
+  console.log("Looking for", id, "and found", found);
+  return found;
+}
+
+/**
+ *
+ * @param {string} original
+ * @param {string} lastSeparator
+ * @returns
+ */
+function stringAfter(original, lastSeparator) {
+  return original.substring(original.lastIndexOf(lastSeparator) + 1);
+}
+
 function formatName(request, session) {
-  return `${request.type} - ${request.params.id.replace(
+  return `${request?.type} - ${request?.params?.id.replace(
     `_${session.data.user.email}`,
     ""
   )}`;
@@ -83,7 +180,7 @@ function formatName(request, session) {
 const Flamechart = ({ style, results }) => {
   const { ref, width = 1, height = 1 } = useResizeObserver();
   const session = useSession();
-  const realdata = transform(results, session);
+  const realdata = transform(results, session) || loadingFlameGraph;
 
   return (
     <Paper ref={ref} style={{ ...style }} variant="outlined">
